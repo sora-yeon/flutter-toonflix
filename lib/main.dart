1c8fe47795af7cabd86cf959ef7d6e022c37073e
sryeon <sryeon@fasoo.com>
2023-08-13 12:13:58 +0900

    timer ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:toonflix/screens/home_screen.dart';
 
 void main() {
   runApp(const App());
@@ -16,59 +17,19 @@ class _AppState extends State<App> {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
+        colorScheme: ColorScheme.fromSwatch(
+          backgroundColor: const Color(0xFFE7626C),
+        ),
         textTheme: const TextTheme(
-          titleLarge: TextStyle(
-            color: Colors.red,
+          displayLarge: TextStyle(
+            color: Color(0xFF232B55),
           ),
         ),
+        cardColor: const Color(0xFFF4EDDB),
       ),
       home: const Scaffold(
-        backgroundColor: Color(0xFFF4EDDB),
-        body: Center(
-            child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            MyLargeTitle(),
-          ],
-        )),
+        body: HomeScreen(),
       ),
-    );
-  }
-}
-
-class MyLargeTitle extends StatefulWidget {
-  const MyLargeTitle({
-    super.key,
-  });
-
-  @override
-  State<MyLargeTitle> createState() => _MyLargeTitleState();
-}
-
-class _MyLargeTitleState extends State<MyLargeTitle> {
-  int count = 0;
-
-  @override
-  void initState() {
-    super.initState();
-    // state가 초기화될 때 호출되는 메서드
-    // 최초 한번만 호출되며, build보다 먼저 호출된다.
-    // 보통은 사용하지 않으나, 부모의 데이터를 state에 초기화 할 때 사용함.
-  }
-
-  @override
-  void dispose() {
-    // 위젯이 사라질 때 호출되는 메서드
-    // API 업데이트나, 이벤트 리스너의 구독 취소 등.. 무언가를 취소할 때 사용함.
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Text(
-      'My Lare Title',
-      style: TextStyle(
-          fontSize: 30, color: Theme.of(context).textTheme.titleLarge!.color),
     );
   }
 }
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Theme.of(context).colorScheme.background,
+        body: Column(
+          children: [
+            Flexible(
+              flex: 1,
+              child: Container(
+                alignment: Alignment.bottomCenter,
+                child: const Text(
+                  '25:00',
+                  style: TextStyle(
+                    // color: Theme.of(context).textTheme.displayLarge.color,
+                    fontSize: 80,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ),
+            ),
+            Flexible(
+              flex: 3,
+              child: Center(
+                child: IconButton(
+                  onPressed: () {},
+                  iconSize: 120,
+                  color: Theme.of(context).cardColor,
+                  icon: const Icon(Icons.play_circle_outline),
+                ),
+              ),
+            ),
+            Flexible(
+              flex: 1,
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      decoration:
+                          BoxDecoration(color: Theme.of(context).cardColor),
+                      child: const Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Pomodoros',
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.w600,
+                              // color: Theme.of(context)
+                              //     .textTheme
+                              //     .displayLarge!
+                              //     .color),
+                              color: Color(0xFF232B55),
+                            ),
+                          ),
+                          Text(
+                            '0',
+                            style: TextStyle(
+                              fontSize: 58,
+                              fontWeight: FontWeight.w600,
+                              color: Color(0xFF232B55),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ));
+  }
+}
